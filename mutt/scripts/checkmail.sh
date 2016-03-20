cc8bbaf651291c26fc90418cbbfc250866aa18ee
Espen Henriksen <dev@henriksen.is>
2016-03-20 15:56:16 +0100

    Add i3 predefined PIM workspace

--- mutt/scripts/checkmail.sh
+++ mutt/scripts/checkmail.sh
@@ -5,7 +5,7 @@ STATE="$(nmcli networking connectivity)"
 if [ $STATE = 'full' ]
 then
     # /usr/local/bin/msmtp-runqueue.sh
-    mbsync -qq -a
+    mbsync -q -a
     exit 0
 fi
 echo "No internet connection."
