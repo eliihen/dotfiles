c6f50f49e550127b9a0706432ddc5ce19cd14749
Espen Henriksen <dev@henriksen.is>
2016-03-28 03:44:14 +0200

    Update install scripts and add OMZ

--- vim/install.sh
+++ vim/install.sh
@@ -8,14 +8,15 @@ popd > /dev/null
 
 ln -vs $DIR/.vimrc $HOME/.vimrc
 
+mkdir -p ~/.vim/{colors,bundle}
+wget https://raw.githubusercontent.com/queyenth/oxeded.vim/master/colors/oxeded.vim -O ~/.vim/colors/oxeded.vim
+
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-mkdir -p ~/.vim/colors/
-wget https://raw.githubusercontent.com/queyenth/oxeded.vim/master/colors/oxeded.vim -O ~/.vim/colors/oxeded.vim
-
 cd ~/.vim/bundle/YouCompleteMe
-python2 install.py --clang-completer --tern-completer
+python3 install.py --clang-completer --tern-completer
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
--- zsh/install.sh
+++ zsh/install.sh
@@ -6,5 +6,9 @@ pushd `dirname $0` > /dev/null
 DIR=`pwd`
 popd > /dev/null
 
+# Install OMZ
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm $HOME/.zshrc
 ln -vs $DIR/.zshrc $HOME/.zshrc
 
