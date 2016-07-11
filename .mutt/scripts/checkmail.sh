1d2e24d264c2edc1fe7e2ef25a7085bd2ddbd413
Espen Henriksen <dev@henriksen.is>
2016-07-11 19:43:33 +0200

    Replace symlinks to dotfiles-old with actual files

--- .mutt/scripts/checkmail.sh
+++ .mutt/scripts/checkmail.sh
@@ -1,1 +1,13 @@
-/home/espen/workspace/espen/dotfiles/mutt/scripts/checkmail.sh
+#!/bin/sh
+
+STATE="$(nmcli networking connectivity)"
+
+if [ $STATE = 'full' ]
+then
+    # /usr/local/bin/msmtp-runqueue.sh
+    mbsync -q -a
+    exit 0
+fi
+echo "No internet connection."
+exit 0
+
