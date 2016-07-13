0b5d6d7d29e635c2c2b3abe1e778704a3938e36d
Espen Henriksen <dev@henriksen.is>
2016-07-13 23:02:00 +0200

    Copy getPass into repository

--- .vdirsyncer/getPass.sh
+++ .vdirsyncer/getPass.sh
@@ -1,1 +1,7 @@
-/home/espen/workspace/espen/dotfiles/mutt/scripts/getPass.sh
+#!/bin/bash
+# This uses the pass executable to get the
+# stored password. The passwords are stored
+# GPG-encrypted in the pass store.
+
+pass "$1" | head -n 1
+
