acdc864b5e449d84f25155f99ff1cfcfc19f3578
Espen Henriksen <dev@henriksen.is>
2016-07-16 02:41:35 +0200

    Add racer completer to vim setup

--- .vim/install.sh
+++ .vim/install.sh
@@ -7,7 +7,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clang-completer --tern-completer
+python3 install.py --clang-completer --tern-completer --racer-completer
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
