d1e5daf759802fe9170cd5d713857545ca8bb9c4
Espen Henriksen <dev@henriksen.is>
2017-01-02 20:39:37 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,21 +11,14 @@ fish_vi_key_bindings                # Use vi key bindings
 #  Oh my fish plugins  #
 ########################
 
-# It kind of sucks that there is no pj for fish..
-# Recreate pj with jump by using this script
-set PROJECT_PATHS \
-  $HOME/workspace/oms \
-  $HOME/workspace/vps \
-  $HOME/workspace/espen \
-  $HOME/workspace/diverse
-function update_markpath
-  for directory in (find $PROJECT_PATHS -maxdepth 1 -type d)
-      cd $directory
-      mark (basename $directory) $directory
-  end
-end
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-set -gx MARKPATH $HOME/.config/markpath  # Marks for jump
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
+set PROJECT_PATHS $HOME/workspace/*
 
 
 ###########################
@@ -49,10 +42,18 @@ set -gx EDITOR nvim
 set -gx LANG en_US.UTF-8
 set -gx BROWSER firefox-nightly
 set -gx TERM rxvt-unicode-256color
+set -gx COLORTERM rxvt-unicode-256color
+
+set -gx COLLECT_HOME /local/collect/collect-6.12.9
+set -gx COLLECT_DOMAIN /local/home/c_sben/domain
 
 # Work VPN is old
 set -gx NSS_HASH_ALG_SUPPORT +MD5
 set -gx OPENSSL_ENABLE_MD5_VERIFY 1
+
+# GTK HiDPI
+# TODO: Only for fenrir
+set -gx GTK_DPI_SCALE 1.2
 
 # Fix java apps for non-re-parenting window managers (like sway)
 set -gx _JAVA_AWT_WM_NONREPARENTING 1
@@ -67,33 +68,34 @@ set -gx PASSWORD_STORE_DIR $HOME/ownCloud/Documents/passwords/password_store
 #  Abbrebiations  #
 ###################
 
-abbr -a "work cd $HOME/workspace/oms"
-abbr -a "j jump"
-abbr -a "s sudo"
+abbr -a work cd $HOME/workspace/oms
+abbr -a j pj
+abbr -a jo pj open
+abbr -a s sudo
 
 # Git abbreviations
-abbr -a "g git"
-abbr -a "gst git status"
-abbr -a "glg git log --stat --show-signature"
-abbr -a "ga git add"
-abbr -a "gd git diff"
-abbr -a "gds git diff --staged"
-abbr -a "gu git up"
-abbr -a "gcm git checkout master"
-abbr -a "gcd git checkout develop"
-abbr -a "gc- git checkout -"
+abbr -a g git
+abbr -a gst git status
+abbr -a glg git log --stat --show-signature
+abbr -a ga git add
+abbr -a gd git diff
+abbr -a gds git diff --staged
+abbr -a gu git up
+abbr -a gcm git checkout master
+abbr -a gcd git checkout develop
+abbr -a gc- git checkout -
 
 # yadm abbreviations
-abbr -a "yst yadm status"
-abbr -a "ya yadm add"
-abbr -a "yd yadm diff"
-abbr -a "yds yadm diff --staged"
-abbr -a "yc yadm commit"
-abbr -a "yp yadm push"
+abbr -a yst yadm status
+abbr -a ya yadm add
+abbr -a yd yadm diff
+abbr -a yds yadm diff --staged
+abbr -a yc yadm commit
+abbr -a yp yadm push
 
-abbr -a "java8 set -gx JAVA_HOME /local/java/jdk1.8.0_51"
-abbr -a "java7 set -gx JAVA_HOME /local/java/jdk1.7.0_79"
-abbr -a "java6 set -gx JAVA_HOME /local/java/jdk1.6.0_31"
+abbr -a java8 set -gx JAVA_HOME /local/java/jdk1.8.0_51
+abbr -a java7 set -gx JAVA_HOME /local/java/jdk1.7.0_79
+abbr -a java6 set -gx JAVA_HOME /local/java/jdk1.6.0_31
 
 
 #############
